f8cbb01191f46d5f201615a30ac584bac8d71e31
DAM <dam@A22O06.local>
2018-09-25 13:13:54 +0200

    rc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  saludo = 'Hola loka';
+  estado = 'alerta';
+
+  showElement=true;
+
+  arrayEstados=['triste', 'alegre', 'motivado'];
+  clickEstado(k, nomEstado){
+    console.log(nomEstado);
+    this.arrayEstados[k]= 'cansado';
+  }
+  showHideTitle(){
+    this.showElement=!this.showElement;
+  }
+
+  changeEstado(){
+    if(this.estado == 'alerta'){
+      this.estado = 'info';
+    }else{
+      this.estado='alerta';
+    }
+  }
 }
