8fc72a6a6c797d0d385ab6c7ee3c1dd88fa07e3b
DAM <dam@A22O06.local>
2018-09-26 13:05:47 +0200

    Primera part Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,52 @@ export class AppComponent {
   title = 'angular';
   saludo = 'Hola loka';
   estado = 'alerta';
+  color = 'amarillo';
 
-  showElement=true;
+  showElement = true;
 
-  arrayEstados=['triste', 'alegre', 'motivado'];
-  clickEstado(k, nomEstado){
+  arrayEstados = ['triste', 'alegre', 'motivado'];
+  arrayButtons = ['boto1', 'boto2', 'boto3'];
+  
+  clickEstado(k, nomEstado) {
     console.log(nomEstado);
-    this.arrayEstados[k]= 'cansado';
+    this.arrayEstados[k] = 'cansado';
   }
-  showHideTitle(){
-    this.showElement=!this.showElement;
+  showHideTitle() {
+    this.showElement = !this.showElement;
   }
 
-  changeEstado(){
-    if(this.estado == 'alerta'){
-      this.estado = 'info';
-    }else{
-      this.estado='alerta';
+  colors = ['azul', 'rojo', 'verde', 'amarillo'];
+  start = ['amarillo', 'amarillo', 'amarillo']
+
+  changeColors(k) {
+    switch (this.start[k]) {
+      case this.colors[0]: {
+        this.start[k] = this.colors[1];
+        console.log("rojo");
+        break;
+      }case this.colors[1]:{
+        this.start[k] = this.colors[2];
+        break;
+      }case this.colors[2]:{
+        this.start[k] = this.colors[3];
+        break;
+      }case this.colors[3]:{
+        this.start[k] = this.colors[0];
+        break;
+      }
     }
   }
+
+
+  changeEstado() {
+    if (this.estado == 'alerta') {
+      this.estado = 'info';
+    } else {
+      this.estado = 'alerta';
+    }
+  }
+
+
+
 }
